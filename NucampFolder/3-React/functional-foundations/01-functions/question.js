9654f53be89ef3a53fdf2ab83b420085e91b6bf1
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-05-05 16:51:50 -0500

    fixes and week 1

---
+++ NucampFolder/3-React/functional-foundations/01-functions/question.js
@@ -0,0 +1,14 @@
+// challenge
+const message = "your phone number is contained somewhere in the digits of Pi";
+const surprisedReaction = () => console.log(":o");
+const smoothReaction = () => console.log("B)");
+const saySomethingAndReact = (msg = "you forgot to submit a message") => {
+  console.log(msg);
+  // this function is returning another function (reaction())
+  return (reaction) => {
+    reaction();
+  };
+};
+
+// add the inputs here
+saySomethingAndReact(message)(surprisedReaction);
