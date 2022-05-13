18959f2e428c896de64e7c47186ab3db16bd03a0
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-05-12 20:55:07 -0500

    UseParams

--- NucampFolder/3-React/nucampsite/src/pages/CampsitesDirectoryPage.js
+++ NucampFolder/3-React/nucampsite/src/pages/CampsitesDirectoryPage.js
@@ -1,33 +1,10 @@
-import { Container, Row, Col } from 'reactstrap';
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
-import { useState } from 'react';
-import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 
 const CampsitesDirectoryPage = () => {
-  // using useState
-
-  // selectRandomCampsite sets the initial state
-  // toggleCampsite is what changes/updates the state
-  // the below is for a random campsite
-  //const [selectedCampsite, toggleCampsite] = useState(selectRandomCampsite());
-
-  // to select a campsite by id - initial state is the first campsite in the campsites object
-  const [campsiteId, setCampsiteId] = useState(0);
-  // campsiteId is from campsitesSlice.js
-  const selectedCampsite = selectCampsiteById(campsiteId);
-
   return (
     <Container>
-      <Row>
-        <Col sm='5' md='7'>
-          <CampsitesList setCampsiteId={setCampsiteId} />
-        </Col>
-        <Col sm='7' md='5'>
-          <CampsiteDetail campsite={selectedCampsite} />
-        </Col>
-      </Row>
+      <CampsitesList />
     </Container>
   );
 };
