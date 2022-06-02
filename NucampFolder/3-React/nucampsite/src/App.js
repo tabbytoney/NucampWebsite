ab91a424b57f3996a0c27825c852ee03fc9b55af
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-06-01 20:31:59 -0500

    added ability to get data from json-server

--- NucampFolder/3-React/nucampsite/src/App.js
+++ NucampFolder/3-React/nucampsite/src/App.js
@@ -1,4 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import './App.css';
@@ -9,6 +12,12 @@ import Footer from './components/Footer';
 import AboutPage from './pages/AboutPage';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className='App'>
       <Header />
