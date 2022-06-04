bc0c4313f80841310bd07e971aa46c8413448474
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-06-04 11:00:01 -0500

    workshop

--- NucampFolder/3-React/nucampsite/src/features/comments/CommentForm.js
+++ NucampFolder/3-React/nucampsite/src/features/comments/CommentForm.js
@@ -10,7 +10,7 @@ import {
   Label,
 } from 'reactstrap';
 import { validateCommentForm } from '../../utils/validateCommentForm';
-import { addComment } from './commentsSlice';
+import { postComment } from './commentsSlice';
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -26,7 +26,7 @@ const CommentForm = ({ campsiteId }) => {
       date: new Date(Date.now()).toISOString(),
     };
     console.log(comment);
-    dispatch(addComment(comment));
+    dispatch(postComment(comment));
     setModalOpen(false);
   };
   return (
