44be84a8020bfb72c7776ca07f6cab0e243a5866
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-08-05 18:32:47 -0500

    passport

--- NucampFolder/5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ NucampFolder/5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -11,6 +11,8 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 
@@ -47,24 +49,21 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     const err = new Error('You are not authenticated!');
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === 'authenticated') {
-      return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 
